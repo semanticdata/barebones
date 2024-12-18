3cabadc75278bc49515efe6a0c3143629a977411
semanticdata <contact@miguelpimentel.do>
2024-12-18 10:50:25 -0600

    change base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // change this to your own domain
-  site: "https://barebones-phi.vercel.app/",
+  site: "https://miguelpimentel.do/",
   integrations: [tailwind(), sitemap()],
-  // legacy: {
-  //   collections: true
-  // }
 });
