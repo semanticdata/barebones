0d8b944424b8dbf932e088c47960b03446a0955f
semanticdata <contact@miguelpimentel.do>
2025-07-03 14:01:24 -0500

    add eslint-plugin-jsx-a11y

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
+    plugins: { 'jsx-a11y': jsxA11y },
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    }
-  }
+      ...jsxA11y.configs.recommended.rules,
+    },
+  },
 ];
