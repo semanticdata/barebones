3c55b33d00400121fc8e5d9dab03dc61030fb16f
semanticdata <contact@miguelpimentel.do>
2025-07-03 14:27:26 -0500

    format and lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
+import eslintPluginAstro from "eslint-plugin-astro";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
-    plugins: { 'jsx-a11y': jsxA11y },
+    plugins: { "jsx-a11y": jsxA11y },
     rules: {
       ...jsxA11y.configs.recommended.rules,
     },
