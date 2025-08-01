4a9b1b289db7a77f32a3303ef601ff6a3e92e243
semanticdata <contact@miguelpimentel.do>
2025-08-01 17:26:23 -0500

    update content collection definition

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,7 @@
-import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blog" }),
+  type: "content",
   schema: ({ image }) =>
     z.object({
       title: z.string(),
